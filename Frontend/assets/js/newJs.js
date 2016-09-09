65cd2f7bf4eac86e8e36c58e3b875d5334324f6d
gcdpinho <gcdpinho@gmail.com>
2016-09-09 03:43:02 -0300

    imagens do slider adaptativas

---
+++ Frontend/assets/js/newJs.js
@@ -0,0 +1,26 @@
+/*
+var changePicture = function(){
+    if (img == nImages)
+        img = 1
+    else
+        img++
+
+    $('header').css("background-image", "url(Content/img/Danilo/Danilo"+img+".jpg)");
+    setTimeout(changePicture, 4000);
+}
+img = 0
+nImages = 4
+//setTimeout(changePicture, 4000);
+*/
+
+height = $(window).height();
+$('.slider-img').css('height', height);
+
+var changeHeight = function(){
+    if ($(window).height() != height)
+        height = $(window).height();
+        $('.slider-img').css('height', height);
+    setTimeout(changeHeight, 300);
+}
+
+changeHeight();
