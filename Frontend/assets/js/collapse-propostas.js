7b704c4ccda2d4404ec0c0cd9a1e0394374aedee
jeanrantunes <jeanrantunes93@gmail.com>
2016-09-10 02:44:09 -0300

    Ajuste propostas

--- Frontend/assets/js/collapse-propostas.js
+++ Frontend/assets/js/collapse-propostas.js
@@ -4,11 +4,11 @@
 		var $selector = $(this);
 		if($selector.attr("aria-expanded") == "true"){ 
 			$selector.find(".sprite").removeClass("icon-top").addClass("icon-bottom");
-			$selector.find(".panel-heading").removeClass("bg-grey").children("h4").removeClass("ft-white");
+			$selector.find(".panel-heading").removeClass("bg-grey").find("h4").removeClass("ft-white");
 		}
 		else {
 			$selector.find(".sprite").removeClass("icon-bottom").addClass("icon-top");
-			$selector.find(".panel-heading").addClass("bg-grey").children("h4").addClass("ft-white");	
+			$selector.find(".panel-heading").addClass("bg-grey").find("h4").addClass("ft-white");	
 		}
 	});
 }());
