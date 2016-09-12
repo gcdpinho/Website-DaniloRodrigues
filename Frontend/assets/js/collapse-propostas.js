c1cefc625a5090117a44d2a1cbcff928a63f7a76
jeanrantunes <jeanrantunes93@gmail.com>
2016-09-12 18:28:23 -0300

    Ajustes

--- Frontend/assets/js/collapse-propostas.js
+++ Frontend/assets/js/collapse-propostas.js
@@ -1,15 +1,15 @@
 (function(){
 	$(".collapsed").on("click", function(){
-		$(".panel-heading").removeClass("bg-grey").find("h4").removeClass("ft-white");
+		$(".panel-heading").removeClass("bg-blue").find("h4").removeClass("ft-white");
 		$(".panel-heading").find(".sprite").removeClass("icon-top").addClass("icon-bottom");
 		var $selector = $(this);
 		if($selector.attr("aria-expanded") == "true"){ 
 			$selector.find(".sprite").removeClass("icon-top").addClass("icon-bottom");
-			$selector.find(".panel-heading").removeClass("bg-grey").find("h4").removeClass("ft-white");
+			$selector.find(".panel-heading").removeClass("bg-blue").find("h4").removeClass("ft-white");
 		}
 		else {
 			$selector.find(".sprite").removeClass("icon-bottom").addClass("icon-top");
-			$selector.find(".panel-heading").addClass("bg-grey").find("h4").addClass("ft-white");	
+			$selector.find(".panel-heading").addClass("bg-blue").find("h4").addClass("ft-white");	
 		}
 	});
 }());
