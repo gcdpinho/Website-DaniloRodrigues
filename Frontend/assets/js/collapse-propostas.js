143be5cd66c1bccb3f576b1c455b37d2b5990f2d
gcdpinho <gcdpinho@gmail.com>
2016-09-13 07:47:47 -0300

    arte no slider

--- Frontend/assets/js/collapse-propostas.js
+++ Frontend/assets/js/collapse-propostas.js
@@ -3,13 +3,13 @@
 		$(".panel-heading").removeClass("bg-blue").find("h4").removeClass("ft-white");
 		$(".panel-heading").find(".sprite").removeClass("icon-top").addClass("icon-bottom");
 		var $selector = $(this);
-		if($selector.attr("aria-expanded") == "true"){ 
+		if($selector.attr("aria-expanded") == "true"){
 			$selector.find(".sprite").removeClass("icon-top").addClass("icon-bottom");
 			$selector.find(".panel-heading").removeClass("bg-blue").find("h4").removeClass("ft-white");
 		}
 		else {
 			$selector.find(".sprite").removeClass("icon-bottom").addClass("icon-top");
-			$selector.find(".panel-heading").addClass("bg-blue").find("h4").addClass("ft-white");	
+			$selector.find(".panel-heading").addClass("bg-blue").find("h4").addClass("ft-white");
 		}
 	});
 }());
